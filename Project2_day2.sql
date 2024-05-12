9c8e00b0d217c3349aa3f927127c3e6bca7b6ae2
Rena Nguyen <167734045+RenaNguyen1997@users.noreply.github.com>
2024-05-12 22:28:10 +1000

    Update Project2_day2.sql

--- Project2_day2.sql
+++ Project2_day2.sql
@@ -1,31 +1,29 @@
---Original data for col1, col2
--- select 
---   format_datetime('%Y-%m',od.created_at) as month,
---   format_datetime('%Y', od.created_at) as Year,
---   pd.category as Product_category
--- from bigquery-public-data.thelook_ecommerce.orders as od
--- join bigquery-public-data.thelook_ecommerce.order_items as oi on od.order_id = oi.order_id
--- join bigquery-public-data.thelook_ecommerce.products as pd on oi.product_id = pd.id
--- order by year, month
-
-
---Col3,col4, monthly revenue by category: TPV, TPO
 With revenue as (
 SELECT 
   format_datetime('%Y-%m', oi.created_at) as yearmonth, 
+  format_datetime('%Y', oi.created_at) as Year,
   pr.category as Product_category,
   count(distinct oi.order_id) as TPO,
-  round(sum(oi.sale_price),2) as TPV
+  round(sum(oi.sale_price),2) as TPV,
+  round(sum(pr.cost),2) as Total_cost,
+  round(sum(oi.sale_price) - sum(pr.cost),2) as Total_profit,
+  round((sum(oi.sale_price) - sum(pr.cost))/sum(pr.cost),2) as Profit_to_cost_ratio
 FROM `bigquery-public-data.thelook_ecommerce.order_items` as oi
 join bigquery-public-data.thelook_ecommerce.products as pr
 on oi.product_id = pr.id
-group by format_datetime('%Y-%m', oi.created_at), pr.category
+group by format_datetime('%Y', oi.created_at),format_datetime('%Y-%m', oi.created_at), pr.category
 order by Product_category, yearmonth
 )
 select
   yearmonth,
+  year,
   Product_category, 
   TPV,
-  lag(TPV) over(order by Product_category, yearmonth ) as previous_month_TPV
+  round((100*(TPV-lag(TPV) over(order by Product_category, yearmonth )))/TPV,2) || '%' as Revenue_growth,
+  TPO,
+  round(100*((TPO- lag(TPO) over(order by Product_category, yearmonth ))/TPO),2) ||'%' as Order_growth,
+  Total_cost, 
+  Total_profit,
+  Profit_to_cost_ratio
 from revenue
 order by Product_category, yearmonth
