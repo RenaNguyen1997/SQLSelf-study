33db0c07653b65b5783cab7953692a98b3fb209d
Rena Nguyen <167734045+RenaNguyen1997@users.noreply.github.com>
2024-06-05 01:48:16 +1000

    Update Project2.sql

--- Project2.sql
+++ Project2.sql
@@ -1,12 +1,16 @@
 -- Total customer and order in each month 
 SELECT 
-  format_datetime('%Y-%m',created_at) as order_time,
-  count(distinct user_id) as total_customer,
-  count(order_id) as total_order
-FROM `bigquery-public-data.thelook_ecommerce.orders` 
-group by format_datetime('%Y-%m',created_at)
+  format_datetime('%Y-%m',t1.created_at) as order_time,
+  count(distinct t1.user_id) as total_customer,
+  count(t1.order_id) as total_order
+from bigquery-public-data.thelook_ecommerce.orders as t1
+Join bigquery-public-data.thelook_ecommerce.order_items as t2 
+on t1.order_id=t2.order_id
+Where t1.status='Complete' and 
+t2.delivered_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00' 
+group by format_datetime('%Y-%m',t1.created_at)
 order by order_time
-LIMIT 1000 
+LIMIT 1000
 -- Revenue and customer, in general, increase by time
   
 --avg revenue and total customer each month
@@ -15,7 +19,8 @@ select
   round(sum(sale_price)/ count(order_id),2) as avg_revenue,
   count(distinct user_id) as total_customer  
 from bigquery-public-data.thelook_ecommerce.order_items
-where not status in ('Returned', 'Processing', 'Cancelled')
+where (not status in ('Returned', 'Processing', 'Cancelled'))
+  and (created_at BETWEEN '2019-01-01 00:00:00' AND '2022-05-01 00:00:00')
 group by format_datetime('%Y-%m',created_at)
 order by order_time
 
@@ -62,7 +67,7 @@ where format_datetime('%Y-%m', created_at) between '2019-01' and '2022-04'
 ) as max_age_table
 where age = max_age
 )
-select tag, gender, count(tag)
+select tag, gender, count(tag) as total
 from age_calculation
 group by tag, gender
 
@@ -73,27 +78,22 @@ SELECT
   format_datetime('%Y-%m', oi.created_at) as yearmonth, 
   oi.product_id as product_id, 
   pr.name as product_name,
-  round(sum(pr.cost),2) as cost,
+  round(sum(pr.cost), 2) as cost,
   round(sum(oi.sale_price),2) as sales,
   round(sum(oi.sale_price - pr.cost),2) as profit,
 FROM `bigquery-public-data.thelook_ecommerce.order_items` as oi
 join bigquery-public-data.thelook_ecommerce.products as pr
 on oi.product_id = pr.id
+where oi.status='Complete'
 group by format_datetime('%Y-%m', oi.created_at), oi.product_id, pr.name
 ), ranking as(
 select *,
   dense_rank() over (partition by yearmonth order by profit desc) as rank_product
 from product_revenue
 )
-select 
-  yearmonth,
-  product_id,
-  product_name,
-  cost,
-  sales,
-  profit
+select *
 from ranking
-where rank_product<=5
+where ranking.rank_product<=5
 order by yearmonth, profit desc
 
 --Daily revenune by category in the last three months
@@ -107,5 +107,8 @@ on pr.id = oi.product_id
 where date_diff(cast(format_datetime('%Y-%m-%d', oi.created_at) as date), cast('2022-04-15' as date), month) =3
 group by format_datetime('%Y-%m-%d', oi.created_at), pr.category
 order by dates
+
+
+
 
 
